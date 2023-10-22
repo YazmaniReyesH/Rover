02644a2f923f4fdf3aff8ee11a27d5ee093c40b1
Yazmani Reyes Hernandez <yazmani.reyesh@gmail.com>
2023-10-22 04:51:38 -0600

    Se agregan los últimos archivos de configuracion del proyecto.

---
+++ src/test/java/yazmani/reyesh/rover/RoverApplicationTests.java
@@ -0,0 +1,13 @@
+package yazmani.reyesh.rover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
